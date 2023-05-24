36bfa78e659fe219ae1e1ef500f87c613fea1f13
ponchix <ponchorc28@gmail.com>
2023-05-24 13:23:07 -0600

    tabla usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaUsuariosComponent } from './tabla-usuarios/tabla-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaUsuariosComponent
   ],
   imports: [
     BrowserModule,
