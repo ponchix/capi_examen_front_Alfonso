d8199b49b952ed17fcd8ad165757b255bacab147
ponchix <ponchorc28@gmail.com>
2023-05-24 13:49:31 -0600

    Muestra de resultados

--- src/app/tabla-usuarios/tabla-usuarios.component.ts
+++ src/app/tabla-usuarios/tabla-usuarios.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-tabla-usuarios',
   templateUrl: './tabla-usuarios.component.html',
   styleUrls: ['./tabla-usuarios.component.css']
 })
-export class TablaUsuariosComponent {
+export class TablaUsuariosComponent implements OnInit {
+  users: any[] = [];
 
+  constructor(private userService: UserServiceService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(
+      (response: any) => {
+        this.users = response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TablaUsuariosComponent } from '../tabla-usuarios/tabla-usuarios.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TablaUsuariosComponent],
   imports: [
     CommonModule
   ]
